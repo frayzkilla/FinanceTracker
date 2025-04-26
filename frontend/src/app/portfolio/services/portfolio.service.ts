56fb2470d071c8a29e81490d6ee8987d40370373
frayzkilla <frayzthamotion@gmail.com>
2025-04-26 20:41:41 +0800

    frontend - fixed bugs and updated design

--- frontend/src/app/portfolio/services/portfolio.service.ts
+++ frontend/src/app/portfolio/services/portfolio.service.ts
@@ -2,30 +2,36 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PortfolioService {
-
   private stocksList: any[] = [];
   private getStocksUrl = 'http://localhost:3000/stocks/getlist';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   loadStockSymbols() {
-    this.stocksList = ['BTC', 'ETH', 'AAPL', 'GOOGL', 'AMZN'];
+    this.stocksList = [
+      { code: 'BTC', name: 'Bitcoin' },
+      { code: 'ETH', name: 'Ethereum' },
+      { code: 'AAPL', name: 'Apple Inc.' },
+      { code: 'GOOGL', name: 'Alphabet Inc.' },
+      { code: 'AMZN', name: 'Amazon.com, Inc.' },
+    ];
     // this.http.get<any[]>(this.getStocksUrl).subscribe(data => {
     //   this.stocksList = data;
     // });
   }
 
   searchStockSymbols(query: string) {
-    return this.stocksList.filter(symbol => 
-      symbol.code.includes(query.toUpperCase()) || 
-      symbol.name.toLowerCase().includes(query.toLowerCase())
+    return this.stocksList.filter(
+      (symbol) =>
+        symbol.code.includes(query.toUpperCase()) ||
+        symbol.name.toLowerCase().includes(query.toLowerCase())
     );
   }
 
   async isSymbolValid(symbol: string) {
-    return this.stocksList.some(s => s.code === symbol);
+    return this.stocksList.some((s) => s.code === symbol);
   }
 }
