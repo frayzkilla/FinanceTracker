566fb57ef8dbcc415f585156b4f77771806ea264
frayzkilla <frayzthamotion@gmail.com>
2025-04-26 21:29:20 +0800

    frontend - fixed bug (saving jwt token), added printing username in navbar

--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { mapTo, Observable, tap } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
@@ -10,21 +11,36 @@ export class AuthService {
   private tokenKey = 'access_token';
   private loginUrl = 'http://localhost:3000/auth/login';
   private registerUrl = 'http://localhost:3000/auth/register';
+  userEmail: string | null = null;
+
+
   constructor(
     private http: HttpClient,
     @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
-
-  login(credentials: { email: string; password: string }): Observable<void> {
-    return this.http.post<{ token: string }>(this.loginUrl, credentials).pipe(
-      tap((response) => localStorage.setItem(this.tokenKey, response.token)),
-      mapTo(void 0)
-    );
+  ) {
+    this.loadUserFromToken();
+    console.log(this.getToken())
   }
 
-  register(credentials: { email: string; password: string }): Observable<void> {
-    return this.http.post<{ token: string }>(this.registerUrl, credentials).pipe(
-      tap((response) => localStorage.setItem(this.tokenKey, response.token)),
+  private loadUserFromToken() {
+    const token = this.getToken();
+    if (token && token.split('.').length === 3) {
+      try {
+        const decoded: any = jwtDecode(token);
+        this.userEmail = decoded.username;
+      } catch (error) {
+        console.error('Ошибка декодирования токена', error);
+      }
+    }
+  }
+
+  login(credentials: { email: string; password: string }): Observable<void> {
+    return this.http.post<{ access_token: string }>(this.loginUrl, credentials).pipe(
+      tap((response) => {
+        localStorage.setItem(this.tokenKey, response.access_token);
+        console.log(jwtDecode(response.access_token))
+        this.loadUserFromToken(); // после логина тоже грузим email
+      }),
       mapTo(void 0)
     );
   }
@@ -32,8 +48,17 @@ export class AuthService {
   logout() {
     if (isPlatformBrowser(this.platformId)) {
       localStorage.removeItem(this.tokenKey);
+      this.userEmail = null; // при выходе обнуляем email
     }
   }
+
+  register(credentials: { email: string; password: string }): Observable<void> {
+    return this.http.post<{ access_token: string }>(this.registerUrl, credentials).pipe(
+      tap((response) => localStorage.setItem(this.tokenKey, response.access_token)),
+      mapTo(void 0)
+    );
+  }
+
 
   getToken(): string | null {
     if (isPlatformBrowser(this.platformId)) {
@@ -47,3 +72,4 @@ export class AuthService {
     return !!this.getToken();
   }
 }
+
